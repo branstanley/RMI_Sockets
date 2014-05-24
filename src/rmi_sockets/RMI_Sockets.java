21a3618ea6e07599479ddffd9f73a87c4454133d
branstanley <bstanley@lakeheadu.ca>
2014-05-24 00:23:51 -0400

    Nothing

--- src/rmi_sockets/RMI_Sockets.java
+++ src/rmi_sockets/RMI_Sockets.java
@@ -1,19 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rmi_sockets;
 
-/**
- *
- * @author Outsider
- */
 public class RMI_Sockets {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
